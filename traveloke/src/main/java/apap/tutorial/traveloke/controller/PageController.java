fa4c81c97889329691689b10265d06a4b2177df8
Rifqi Hilman <rifqihilmans21@gmail.com>
2020-11-26 21:14:11 +0700

    feat: new user only visible to admin

--- traveloke/src/main/java/apap/tutorial/traveloke/controller/PageController.java
+++ traveloke/src/main/java/apap/tutorial/traveloke/controller/PageController.java
@@ -1,10 +1,13 @@
 package apap.tutorial.traveloke.controller;
 
 import apap.tutorial.traveloke.service.RoleService;
+import apap.tutorial.traveloke.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class PageController {
@@ -12,8 +15,12 @@ public class PageController {
     @Autowired
     RoleService roleService;
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping("/")
-    private String home(Model model){
+    private String home(Model model, Principal principal){
+        model.addAttribute("role", userService.getUserByUsername(principal.getName()).getRole().getRole());
         model.addAttribute("listRole", roleService.findAll());
         return "home";
     }

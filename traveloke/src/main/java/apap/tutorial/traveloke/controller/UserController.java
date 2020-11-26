ab88ad818c14bc4f42af848edd210c5d226243af
Rifqi Hilman <rifqihilmans21@gmail.com>
2020-11-26 17:43:06 +0700

    feat: new requirements for password

--- traveloke/src/main/java/apap/tutorial/traveloke/controller/UserController.java
+++ traveloke/src/main/java/apap/tutorial/traveloke/controller/UserController.java
@@ -1,6 +1,7 @@
 package apap.tutorial.traveloke.controller;
 
 import apap.tutorial.traveloke.model.UserModel;
+import apap.tutorial.traveloke.service.RoleService;
 import apap.tutorial.traveloke.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,10 +17,33 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleService roleService;
+
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
     public String addUserSubmit(@ModelAttribute UserModel user, Model model){
-        userService.addUser(user);
-        model.addAttribute("user", user);
-        return "redirect:/";
+        if (user.getPassword().length() < 8) {
+            model.addAttribute("msg", "Password harus memiliki minimal 8 karakter");
+            model.addAttribute("listRole", roleService.findAll());
+            return "home";
+        }
+        else {
+            if (user.getPassword().matches(".*[0-9].*") && user.getPassword().matches(".*[a-zA-Z].*")) {
+                userService.addUser(user);
+                model.addAttribute("msg", "User berhasil ditambahkan!");
+                model.addAttribute("listRole", roleService.findAll());
+                return "home";
+            }
+            else if (user.getPassword().matches(".*[0-9].*")){
+                model.addAttribute("msg", "Password harus mengandung minimal 1 huruf");
+                model.addAttribute("listRole", roleService.findAll());
+                return "home";
+            }
+            else {
+                model.addAttribute("msg", "Password harus mengandung minimal 1 angka");
+                model.addAttribute("listRole", roleService.findAll());
+                return "home";
+            }
+        }
     }
 }
